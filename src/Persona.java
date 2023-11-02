7a24b839549cd1b9ada100f8708d3d5b2b74eb5a
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-02 16:38:08 -0300

    create class persona

---
+++ src/Persona.java
@@ -0,0 +1,19 @@
+public class Persona {
+    String nombre;
+    String apellido;
+     int edad;
+
+
+
+public Persona(){
+
+}
+public Persona (String nombre, String apellido, int edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+
+    }
+
+
+}
