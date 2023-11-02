f431ce81e4c2202a63a521113ef57a47fee5bb1a
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-02 16:03:21 -0300

    metodos void and String

--- src/Automovil.java
+++ src/Automovil.java
@@ -2,4 +2,12 @@ public class Automovil {
     String marca;
     String modelo;
     int anioFabricacion;
+
+    //metodos //sout imprime en consola
+    void acelerar(){
+        System.out.println("Acelerando");
+    }
+    String frenar(){
+        return "Frenando";
+    }
 }
