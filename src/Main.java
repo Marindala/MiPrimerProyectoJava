b366b53653c9ea9af61b2f2cd0d75bc76f8ba7f5
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-02 16:29:15 -0300

    current file in consola

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
      Automovil auto1 = new Automovil();
      //lleno //en Java siempre punto y coma
      Automovil auto2 = new Automovil("Renault", "Clio", 2020);
-        System.out.println("Auto 1, marca:" + auto1.marca);
+        System.out.println("Auto 1, año de fabricación:" + auto1.anioFabricacion);
         System.out.println("Auto 2, marca:" + auto2.marca);
     }
 }
