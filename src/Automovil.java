d584f1cac0ee4f05b118deb5f34f34a74d015a01
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-02 16:15:33 -0300

    public

--- src/Automovil.java
+++ src/Automovil.java
@@ -3,6 +3,23 @@ public class Automovil {
     String modelo;
     int anioFabricacion;
 
+    //constructor vacio
+
+    public Automovil(){
+
+    }
+
+    //constructor lleno
+
+    public Automovil(String marca, String modelo, int anioFabricacion){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anioFabricacion = anioFabricacion;
+
+    }
+
+
+
     //metodos //sout imprime en consola
     void acelerar(){
         System.out.println("Acelerando");
