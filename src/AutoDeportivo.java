86d93946e642991506014adcd8a59165b7d531c7
Marina <95050756+Marindala@users.noreply.github.com>
2023-11-03 01:05:28 -0300

    Me gusta Java , me voy a dormir

--- src/AutoDeportivo.java
+++ src/AutoDeportivo.java
@@ -1,9 +1,8 @@
+//Gracias a la Herencia evito copiar tanto codigo
+//con las mismas propiedades que Automovil utilizo extends hereda de Automovil
+//cuando coloco extends que viene de Herencia retiro todo lo que se repite de codigo
+public class AutoDeportivo extends Automovil{
 
-//con las mismas propiedades que Automovil
-public class AutoDeportivo {
-    private String marca;
-    private String modelo;
-    private int anioFabricacion;
 
     //pero puede tener sus propias propiedades
     private boolean esConvertible;
@@ -12,35 +11,12 @@ public class AutoDeportivo {
     }
   //generate con todas las propiedades para un constructor completo
     public AutoDeportivo(String marca, String modelo, int anioFabricacion, boolean esConvertible) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.anioFabricacion = anioFabricacion;
+        //utilizo el super en herencia
+        super(marca, modelo, anioFabricacion);
         this.esConvertible = esConvertible;
     }
    //generate getters y setters para todas las propiedades
-    public String getMarca() {
-        return marca;
-    }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public int getAnioFabricacion() {
-        return anioFabricacion;
-    }
-
-    public void setAnioFabricacion(int anioFabricacion) {
-        this.anioFabricacion = anioFabricacion;
-    }
 
     public boolean isEsConvertible() {
         return esConvertible;
